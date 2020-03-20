1b4227bfae92fad4aca74f378ed37269010c00d7
Aleck Palad <aleckpalad12@gmail.com>
2020-03-20 13:51:29 -0400

    added ship5size

--- src/Battleshp.java
+++ src/Battleshp.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Battleshp {
-    // - use inheritance                                                    //private static String[][] player1placement = new String[6][6];
+    // - use inheritance
     // - ship is abstract base class that the other ships inherit from
     // - encapsulation where every field is private
     // - field only accessible by getter/setter
     // - everything should be an object like the board
-
+    // turn the board to an object, loop over it and then override on player1meth to represent the boats?
     public static void main(String []args){
         System.out.println("Battleship Multiplayer");
         seaboard SEA = new seaboard();
@@ -17,11 +17,14 @@ public class Battleshp {
         player1meth player1 = new player1meth();
 
         player1.player1name();
+                                            //have the player place the biggest ship first so they get
+        player1.player1set5();              //a good idea of the space they have left to work around
+        player1.printplayer1placement();
+
         player1.player1set3();
         player1.printplayer1placement();
 
-        //player1.player1set3();
-        //player1.printplayer1placement();
+
 
 
 
--- src/player1meth.java
+++ src/player1meth.java
@@ -6,30 +6,93 @@ public class player1meth extends seaboard {
     private static String[][] player1placement = new String[6][6];
     boolean placeagain = true;
 
-    //player1attacking board
-    // ------------------------------------------------------------------
-    //method to be asked to place ships?
-        //method for player2 to place
-    //method to chose a tile to attack
-        //method for player 2 to attack
 
     public void player1name(){
         System.out.println("Enter Player 1 name: ");
         Scanner scanthing1 = new Scanner(System.in);
         playeronename = scanthing1.nextLine();
     }
-    //have this be for the 3size boat, need a 1 size, a 5size, similar methods
 
-    public void player1set5(){
 
-    }
-
-    public boolean player1set3(){
-
+    //SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP//SIZE 5 SHIP
+    public boolean player1set5(){
 
         for(int i = 0; i<player1placement.length; i++){             //fill the board with "filler" if theres no "p1" on it
             for(int j=0; j<player1placement.length;j++){
-                if(((i!=0) || (i!=0)) && (player1placement[i][j] != "x")){
+                if(player1placement[i][j] != "x"){
+                    player1placement[i][j] = "~";
+                }
+                if(i==0){
+                    player1placement[0][j] = Integer.toString(j);
+                }
+                if(j==0){
+                    player1placement[i][0] = Integer.toString(i);
+                }
+                if(i==0 && j==0){
+                    player1placement[i][j] = " ";
+                }
+
+            }
+        }
+
+        while(placeagain = true){
+            System.out.println("Player -" + playeronename + "- enter the coordinates for ship#1 which takes 5 tiles, THIS COORDINATE WILL BE THE CENTER OF THE SHIP");
+            System.out.println("-" + playeronename + "- enter the x coordinate");
+            Scanner playeronescannerx = new Scanner(System.in);
+            int playeronesXinput = playeronescannerx.nextInt();
+
+            System.out.println("-" + playeronename + "- enter the y coordinate");
+            Scanner playeronescannery = new Scanner(System.in);
+            int playeronesYinput = playeronescannery.nextInt();
+
+            System.out.println("-" + playeronename + "- horizontal - press 1, or vertical | press 2");
+            Scanner orientation = new Scanner(System.in);
+            int playeronevh = orientation.nextInt();
+
+            //-----
+            if(player1placement[playeronesXinput][playeronesYinput] == "~"){    //if the chosen center coord is 'sea', let this happen
+
+                if(playeronevh == 1){   //check if sideways has room, the extends need to be able to replace the 'sea' tiles in this instance 5 SHIPTILE NEEDS TO CHECK 2 EXTENDS EACH SIDE
+                    if((player1placement[playeronesXinput-1][playeronesYinput] == "~") && (player1placement[playeronesXinput+1][playeronesYinput] == "~")
+                        && (player1placement[playeronesXinput-2][playeronesYinput] == "~") && (player1placement[playeronesXinput+2][playeronesYinput] == "~")){
+                        player1placement[playeronesXinput-1][playeronesYinput] = "x";      //if its within the boundary of the currently 6x6 map replace the board tile with their declared tile
+                        player1placement[playeronesXinput+1][playeronesYinput] = "x";
+                        player1placement[playeronesXinput-2][playeronesYinput] = "x";
+                        player1placement[playeronesXinput+2][playeronesYinput] = "x";
+
+                        player1placement[playeronesXinput][playeronesYinput] = "x";      //let the center replace happen after checking the sides too
+                        placeagain = false;
+                        return placeagain;
+                    }
+                }
+                if(playeronevh == 2){ //check if updown has room for the extends up down
+                    if((player1placement[playeronesXinput][playeronesYinput-1] == "~") && (player1placement[playeronesXinput][playeronesYinput+1] == "~")
+                        && (player1placement[playeronesXinput][playeronesYinput-2]=="~") && (player1placement[playeronesXinput][playeronesYinput+2]=="~")){
+                        player1placement[playeronesXinput][playeronesYinput-1] = "x";
+                        player1placement[playeronesXinput][playeronesYinput+1] = "x";
+                        player1placement[playeronesXinput][playeronesYinput-2] = "x";
+                        player1placement[playeronesXinput][playeronesYinput+2] = "x";
+
+                        player1placement[playeronesXinput][playeronesYinput] = "x";
+                        placeagain = false;
+                        return placeagain;
+                    }
+
+                }
+
+            }
+        }
+
+
+        return placeagain;
+    }
+
+    //SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP//SIZE 3 SHIP
+    public boolean player1set3(){
+
+        for(int i = 0; i<player1placement.length; i++){             //fill the board with "filler" if theres no "p1" on it
+            for(int j=0; j<player1placement.length;j++){
+                if(player1placement[i][j] != "x"){
                     player1placement[i][j] = "~";
                 }
                 if(i==0){
@@ -64,7 +127,7 @@ public class player1meth extends seaboard {
             if(player1placement[playeronesXinput][playeronesYinput] == "~"){    //if the chosen center coord is 'sea', let this happen
 
                 if(playeronevh == 1){   //check if sideways has room, the extends need to be able to replace the 'sea' tiles
-                    if((player1placement[playeronesXinput-1][playeronesYinput] == "~") && (player1placement[playeronesXinput+1][playeronesYinput]) == "~"){
+                    if((player1placement[playeronesXinput-1][playeronesYinput] == "~") && (player1placement[playeronesXinput+1][playeronesYinput]== "~") ){
                         player1placement[playeronesXinput-1][playeronesYinput] = "x";      //if its within the boundary of the currently 6x6 map replace the board tile with their declared tile
                         player1placement[playeronesXinput+1][playeronesYinput] = "x";
 
